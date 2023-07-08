5253c0fc7c076c8f884d583844090002a2c91676
DaivyIsHere <48060061+DaivyIsHere@users.noreply.github.com>
2023-07-08 20:57:56 +0800

    move collectable under object folder

---
+++ object/collectable.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+signal collected
+
+@onready var sprite = get_node("Sprite2D")
+@onready var animation = $CollectableAnimation
+
+func _on_body_entered(body):
+	if body is Penguin:
+		_on_collected()
+	
+func _on_collected():
+	animation.play("collectable_animation")
+	emit_signal("collected")
+	#var tween := create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	#tween.tween_property(self, "position", Vector2.UP * 200, 0.5).as_relative()
+	#tween.parallel().tween_property(sprite, "modulate", Color(1,1,1,0), 0.5)
+	#queue_free()
+	print("COIN!")
+	
