a20f734bcc86d5327b111b1b7a406946bc22d687
IcyLava <icylavah@gmail.com>
2023-07-08 12:40:42 +0300

    Add class name

--- object/penguin.gd
+++ object/penguin.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Penguin
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
