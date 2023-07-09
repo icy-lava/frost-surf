d2a1e97e1054982d3d3eb855ebc671ead57b0562
DaivyIsHere <48060061+DaivyIsHere@users.noreply.github.com>
2023-07-10 00:39:41 +0800

    updated level

--- object/finish_line.gd
+++ object/finish_line.gd
@@ -2,7 +2,15 @@ extends Area2D
 
 @export var transition: ScreenTransition
 @export var transition_scene: PackedScene
+@export var current_scene: PackedScene
+
+@export var fishcount: int
+
+@export var fish_counter: FishCounter
 
 func _on_body_entered(body):
 	if body is Penguin:
-		transition.transition(transition_scene)
+		if fish_counter.count >= fishcount:
+			transition.transition(transition_scene)
+		else:
+			transition.transition(current_scene)
