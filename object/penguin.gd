bcfde0d9365afe7fea43c26508460cbe6450a137
IcyLava <icylavah@gmail.com>
2023-07-09 14:40:32 +0300

    Water boost no water tho

--- object/penguin.gd
+++ object/penguin.gd
@@ -8,6 +8,10 @@ var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 const feather_boost: float = 800
 
 @export var Sprite: Sprite2D
+@export var boost_curve: Curve
+
+const full_boost_time: float = 0.7
+var boost_level: float = 0
 
 func _physics_process(delta: float) -> void:
 	# Physics
@@ -23,6 +27,13 @@ func _physics_process(delta: float) -> void:
 	# Slow down left movement
 	if velocity.x < 0:
 		velocity.x = Game.dampf(velocity.x, 0, 0.3, delta)
+	
+	# Water boost
+	var in_water := global_position.y > Game.get_water_y()
+	boost_level = move_toward(boost_level, 1 if in_water else 0, delta / full_boost_time)
+	var boost_level_adjusted = boost_curve.sample_baked(boost_level)
+#	modulate = lerp(Color.WHITE, Color.BLUE, boost_level_adjusted)
+	velocity.x += 1000 * delta * boost_level_adjusted
 	
 	# Gradual sprite rotation, based on velocity
 	var current_rotation_vector := Vector2.from_angle(Sprite.rotation)
